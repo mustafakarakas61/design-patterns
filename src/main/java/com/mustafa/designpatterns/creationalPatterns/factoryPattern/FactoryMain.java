158af12de2f0b2b7a04c38d80b8c887923c15334
Mustafa karakaş <mustafakarakas61ts862@gmail.com>
2023-09-03 21:59:44 +0300

    Factory cretional pattern up

--- src/main/java/com/mustafa/designpatterns/creationalPatterns/factoryPattern/FactoryMain.java
+++ src/main/java/com/mustafa/designpatterns/creationalPatterns/factoryPattern/FactoryMain.java
@@ -4,7 +4,9 @@ import com.mustafa.designpatterns.creationalPatterns.factoryPattern.enm.FileType
 import com.mustafa.designpatterns.creationalPatterns.factoryPattern.service.FileExporterFactory;
 
 public class FactoryMain {
-
+    /**
+     * Factory ve interface'ler bilinecek sadece
+     */
     public static void main(String[] args) {
         String file = FileExporterFactory.getInstance(FileType.EXCEL).export("Test Content!");
         System.out.println(file);
